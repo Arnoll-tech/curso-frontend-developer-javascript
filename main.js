ea3023a6341dbbaccf98c3a41848cd6fe2dfe953
arnoll-tech <arnolbermudez28@gmail.com>
2022-08-30 11:07:25 -0500

    tercera fusion, agregando aside y condicionales en los eventos

--- main.js
+++ main.js
@@ -2,13 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburgerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const carritoIconMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamburgerMenu.addEventListener('click', toggleMobileMenu);
+carritoIconMenu.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
+   const isDestoktopMenu = menuEmail.classList.contains('inactive');
+
+   if (!isDestoktopMenu) {
+      aside.classList.add('inactive');
+   }
    desktopMenu.classList.toggle('inactive');
 }
+
 function toggleMobileMenu() {
+   const isAsideClosed = aside.classList.contains('inactive');
+
+   if (!isAsideClosed) {
+      aside.classList.add('inactive');
+   }
    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+   if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
+   }else{
+      desktopMenu.classList.add('inactive');
+   }
+   aside.classList.toggle('inactive');
 }
